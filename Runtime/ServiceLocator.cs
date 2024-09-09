5cd272df23a0ae2b15de6d51b864e4e110ae52b4
EOppedal <erlend.oppedal@gmail.com>
2024-09-09 11:55:31 +0200

    RegisteredTypes HashSet

--- Runtime/ServiceLocator.cs
+++ Runtime/ServiceLocator.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 
 public static class ServiceLocator {
     private static readonly Dictionary<Type, object> Services = new();
-    
+    private static readonly HashSet<Type> RegisteredTypes = new();
+
     public static void Register<T>(T service) {
-        Services.Add(typeof(T), service);
+        var type = typeof(T);
+        if (!RegisteredTypes.Contains(type)) {
+            Services.Add(type, service);
+            RegisteredTypes.Add(type);
+        } else {
+            throw new InvalidOperationException($"{type.Name} is already registered.");
+        }
     }
 
     public static void Unregister<T>() {
-        Services.Remove(typeof(T));
+        var type = typeof(T);
+        if (RegisteredTypes.Contains(type)) {
+            Services.Remove(type);
+            RegisteredTypes.Remove(type);
+        }
     }
 
     public static T Get<T>() {
